e7efab76178fc866f9db5effb0e1eeb460718a35
natanael.leite <natanael.leite@serpro.gov.br>
2023-05-03 14:31:13 -0300

    feat: remove o useEffect do componente select e remove tudo

--- src/Components/Select.tsx
+++ src/Components/Select.tsx
@@ -23,8 +23,8 @@ interface Props {
 }
 
 export const Select = (props: Props) => {
-  const [selectCore, setSelectCore] = useState<any>();
-  const brSelectRef = useRef<HTMLDivElement>(null);
+  // const [selectCore, setSelectCore] = useState<any>();
+  // const brSelectRef = useRef<HTMLDivElement>(null);
 
   useEffect(
     () => {
@@ -36,92 +36,89 @@ export const Select = (props: Props) => {
   );
 
   const getItem = () => {
-    if (brSelectRef.current) {
-      const brSelectInput: HTMLInputElement =
-        selectCore?.component.querySelector(".br-input input");
-      const primeiroItemLista: HTMLInputElement =
-        selectCore?.optionsList[0].element;
-
-      if (selectCore?.optionsList[0]) {
-        selectCore.optionsList[0].selected = false;
-      }
-
-      if (typeof selectCore?.selectedValue === "string") {
-        props.onChange && props.onChange(selectCore?.selectedValue);
-      } else {
-        props.onChange && props.onChange("");
-        primeiroItemLista?.removeAttribute("checked");
-        primeiroItemLista?.classList.remove("selected");
-
-        if (brSelectInput?.value) {
-          brSelectInput.value = "";
-        }
-      }
-    }
+    // if (brSelectRef.current) {
+    //   const brSelectInput: HTMLInputElement =
+    //     selectCore?.component.querySelector(".br-input input");
+    //   const primeiroItemLista: HTMLInputElement =
+    //     selectCore?.optionsList[0].element;
+    //   if (selectCore?.optionsList[0]) {
+    //     selectCore.optionsList[0].selected = false;
+    //   }
+    //   if (typeof selectCore?.selectedValue === "string") {
+    //     props.onChange && props.onChange(selectCore?.selectedValue);
+    //   } else {
+    //     props.onChange && props.onChange("");
+    //     primeiroItemLista?.removeAttribute("checked");
+    //     primeiroItemLista?.classList.remove("selected");
+    //     if (brSelectInput?.value) {
+    //       brSelectInput.value = "";
+    //     }
+    //   }
+    // }
   };
 
   const instanciarComponenteSelect = () => {
-    const coreSelect = new core.BRSelect("br-select", brSelectRef.current);
-
-    setSelectCore(coreSelect);
+    // const coreSelect = new core.BRSelect("br-select", brSelectRef.current);
+    // setSelectCore(coreSelect);
   };
 
   return (
-    <div className={`${props.className || ""}`}>
-      <div
-        id={`select-${props.id}`}
-        className="br-select"
-        ref={brSelectRef}
-        onClick={getItem}
-      >
-        <div className="br-input">
-          <label htmlFor={`select-${props.id}-input`}>{props.label}</label>
+    // <div className={`${props.className || ""}`}>
+    //   <div
+    //     id={`select-${props.id}`}
+    //     className="br-select"
+    //     ref={brSelectRef}
+    //     onClick={getItem}
+    //   >
+    //     <div className="br-input">
+    //       <label htmlFor={`select-${props.id}-input`}>{props.label}</label>
 
-          <input
-            type="text"
-            id={`select-${props.id}-input`}
-            placeholder={props.placeholder}
-          />
+    //       <input
+    //         type="text"
+    //         id={`select-${props.id}-input`}
+    //         placeholder={props.placeholder}
+    //       />
 
-          <button
-            className="br-button"
-            type="button"
-            aria-label="Exibir lista"
-            tabIndex={-1}
-            data-trigger="data-trigger"
-          >
-            <i className="fas fa-angle-down" aria-hidden="true" />
-          </button>
-        </div>
+    //       <button
+    //         className="br-button"
+    //         type="button"
+    //         aria-label="Exibir lista"
+    //         tabIndex={-1}
+    //         data-trigger="data-trigger"
+    //       >
+    //         <i className="fas fa-angle-down" aria-hidden="true" />
+    //       </button>
+    //     </div>
 
-        <div className="br-list" tabIndex={0}>
-          <div className="br-item" tabIndex={-1}>
-            <div className="br-radio">
-              <input type="radio" value={-1} id={`-1-${props.id}`} />
-              <label htmlFor={`-1-${props.id}`}>{props.placeholder}</label>
-            </div>
-          </div>
+    //     <div className="br-list" tabIndex={0}>
+    //       <div className="br-item" tabIndex={-1}>
+    //         <div className="br-radio">
+    //           <input type="radio" value={-1} id={`-1-${props.id}`} />
+    //           <label htmlFor={`-1-${props.id}`}>{props.placeholder}</label>
+    //         </div>
+    //       </div>
 
-          {props.data?.map((elemento: any, index: number) => (
-            <div className="br-item" tabIndex={-1} key={index}>
-              <div className="br-radio">
-                <input
-                  id={`${props.id}-${index}`}
-                  type="radio"
-                  value={
-                    elemento[props.valueLabelItems?.value || ""] ||
-                    elemento.codigo
-                  }
-                />
-                <label htmlFor={`${props.id}-${index}`}>
-                  {elemento[props.valueLabelItems?.label || ""] ||
-                    elemento.descricao}
-                </label>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
+    //       {props.data?.map((elemento: any, index: number) => (
+    //         <div className="br-item" tabIndex={-1} key={index}>
+    //           <div className="br-radio">
+    //             <input
+    //               id={`${props.id}-${index}`}
+    //               type="radio"
+    //               value={
+    //                 elemento[props.valueLabelItems?.value || ""] ||
+    //                 elemento.codigo
+    //               }
+    //             />
+    //             <label htmlFor={`${props.id}-${index}`}>
+    //               {elemento[props.valueLabelItems?.label || ""] ||
+    //                 elemento.descricao}
+    //             </label>
+    //           </div>
+    //         </div>
+    //       ))}
+    //     </div>
+    //   </div>
+    // </div>
+    <p>Select</p>
   );
 };
