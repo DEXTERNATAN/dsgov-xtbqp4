06c90fe3f5c410b6becde5a9a17860c5dec3e92c
DEXTERNATAN <pollyanne317@gmail.com>
2023-05-04 16:30:18 -0300

    1 file modified

--- src/Components/Select2.tsx
+++ src/Components/Select2.tsx
@@ -13,10 +13,10 @@ class Select extends Component {
     const selectList = [];
     for (const brSelect of window.document.querySelectorAll('.br-select')) {
       const brselect = new core.BRSelect('br-select', brSelect);
+      console.log('componentDidMount - select');
       brSelect.addEventListener('onChange', (ev) => this.getItem(brselect));
-      // this.brSelect = brselect;
+      this.brSelect = brselect;
       selectList.push(brselect);
-      console.log('Tamanho da lista', selectList.length);
     }
   }
 
